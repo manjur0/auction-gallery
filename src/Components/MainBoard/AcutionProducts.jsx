416f8fc67d38bc0adc07f12c217f412db7bc64bd
manjur0 <sfmanjur21@gmail.com>
2025-04-09 21:38:24 +0600

    Enhance AcutionProducts layout; improve text styling and remove unused elements

--- src/Components/MainBoard/AcutionProducts.jsx
+++ src/Components/MainBoard/AcutionProducts.jsx
@@ -19,22 +19,19 @@ const AcutionProducts = ({ auctionProducts }) => {
         <tbody key={data.id}>
           <tr>
             <td>
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-3 text-lg">
                 <div className="avatar">
                   <div className="mask mask-squircle h-12 w-12">
                     <img src={data.image} alt="Avatar Tailwind CSS Component" />
                   </div>
                 </div>
                 <p className="text-center ">{data.title}</p>
-                <div>
-                  <div className="font-bold"></div>
-                </div>
               </div>
             </td>
-            <td>{data.currentBidPrice}$</td>
-            <td>{data.timeLeft}</td>
+            <td className="text-lg">{data.currentBidPrice}$</td>
+            <td className="text-lg">{data.timeLeft}</td>
             <th>
-              <button className="cursor-pointer text-red-700 ">
+              <button className="cursor-pointer text-red-700 text-xl">
                 <MdFavoriteBorder />
               </button>
             </th>
