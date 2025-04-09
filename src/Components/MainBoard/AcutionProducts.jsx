fc19b56e2169d2f55b5cbdb9faad195990ff1c46
manjur0 <sfmanjur21@gmail.com>
2025-04-09 18:08:26 +0600

    Update auction items in bid.json and enhance MainBoard component layout

---
+++ src/Components/MainBoard/AcutionProducts.jsx
@@ -0,0 +1,9 @@
+// import { use } from "react";
+
+// const AcutionProducts = ({ auctionData }) => {
+//   const auctions = use(auctionData);
+//   console.log(auctions);
+//   return <div></div>;
+// };
+
+// export default AcutionProducts;
