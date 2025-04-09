dbfff066eba453d1a3ffb3d99bbd27b4ad316d4e
manjur0 <sfmanjur21@gmail.com>
2025-04-09 08:15:23 +0600

    Add Hero component and update App structure; modify index.html title

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import bgimage from "../../assets/Banner-min.jpg";
+const Hero = () => {
+  return (
+    <div
+      className="hero min-h-[70vh]"
+      style={{ backgroundImage: `url(${bgimage})` }}
+    >
+      <div className="container  mx-auto flex  bg-transparent text-white ">
+        <div className="hero-content ">
+          <div>
+            <h1 className="text-6xl font-semibold w-3/5">
+              Bid on Unique Items from Around the World
+            </h1>
+            <p className="py-8 text-3xl font-light w-3/5">
+              Discover rare collectibles, luxury goods, and vintage treasures in
+              our curated auctions
+            </p>
+            <button className="btn btn-primary rounded-full bg-white text-black font-semibold text-lg">
+              Explore Auctions
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
